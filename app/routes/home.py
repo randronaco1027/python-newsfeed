44d623ab42e7de3ad371385b598c5c36b5b2c9b5
randronaco <randronaco1027@gmail.com>
2022-09-02 12:03:23 -0400

    finished lesson 3

--- app/routes/home.py
+++ app/routes/home.py
@@ -1,10 +1,17 @@
 from flask import Blueprint, render_template
+from app.models import Post
+from app.db import get_db
 
 bp = Blueprint('home', __name__, url_prefix='/')
 
 @bp.route('/')
 def index():
-    return render_template('homepage.html')
+    db = get_db()
+    posts = db.query(Post).order_by(Post.created_at.desc()).all()
+    return render_template(
+        'homepage.html',
+        posts=posts
+)
 
 @bp.route('/login')
 def login():
@@ -12,4 +19,6 @@ def login():
 
 @bp.route('/post/<id>')
 def single(id):
-    return render_template('single-post.html')
+    db = get_db()
+    post = db.query(Post).filter(Post.id == id).one()
+    return render_template('single-post.html', post=post)
